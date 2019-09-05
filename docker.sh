8ac69dd177185061eb47d698ea46f9fbe7b1464f
Niels Widger <niels@qacafe.com>
2019-09-05 11:19:21 -0400

    Add docker.sh for managing releases using Docker
    Add a new docker.sh script which makes it possible manage releases and
    run cdrouter.py inside a Docker container.  This makes it much simpler
    to test out cdrouter.py in a clean environment using different
    versions of Python.  Note that this commit does not affect users of
    cdrouter.py in any way and is only meant to help maintainers of this
    package.

---
+++ docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IMAGE=python:2
+
+GIT_USER_NAME=$(git config user.name)
+GIT_USER_EMAIL=$(git config user.email)
+
+TWINE_USERNAME=$(grep username ~/.pypirc | sed -e 's/username = //')
+TWINE_PASSWORD=$(grep password ~/.pypirc | sed -e 's/password = //')
+
+if [[ $# -ge 2 && "$1" = "-image" ]];
+then
+    IMAGE=$2
+    shift 2
+fi
+
+WORKDIR=/usr/src/myapp
+
+docker pull ${IMAGE}
+docker run -it --rm -v ${PWD}:${WORKDIR} -w ${WORKDIR} \
+       -e GIT_USER_NAME="${GIT_USER_NAME}"   \
+       -e GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
+       -e TWINE_USERNAME="${TWINE_USERNAME}" \
+       -e TWINE_PASSWORD="${TWINE_PASSWORD}" \
+       ${IMAGE} /usr/src/myapp/manage.sh git-config clean setup "$@"
--- manage.sh
+++ manage.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+function git-config {
+    git config --global user.email "${GIT_USER_EMAIL}" > /dev/null 2>&1
+    git config --global user.name "${GIT_USER_NAME}" > /dev/null 2>&1
+}
+
+function setup {
+    requirements
+    install-editable
+}
+
+function requirements {
+    pip install --no-cache-dir -r requirements.txt
+    pip install --no-cache-dir twine
+}
+
+function install {
+    pip install .
+}
+
+function install-editable {
+    pip install -e .
+}
+
 function publish {
     bump
     build
@@ -10,17 +33,34 @@ function bump {
     previousVersion=$( grep '^__version__ =' cdrouter/__init__.py | sed 's/__version__ = \"\(.*\)\"/\1/' )
     echo "Next version number? (previous: '$previousVersion')"
     read version
+
+    if [ "$version" = "" ];
+    then
+        echo "No version number entered, aborting bump"
+        exit 1
+    fi
+
     sed -i -b "s/__version__ = .*/__version__ = \"$version\"/" cdrouter/__init__.py
 }
 
-function build {
+function clean {
     rm -rf build cdrouter.egg-info dist
+}
+
+function build {
+    clean
     python setup.py sdist bdist_wheel
 }
 
 function push {
-    echo "Break (Ctrl+c) here if something is wrong. Else, press enter"
+    echo "Break (Ctrl+c) or type 'abort' and hit enter if something is wrong. Else, press enter"
     read foobar
+
+    if [ "$foobar" = "abort" ];
+    then
+        echo "User entered 'abort', aborting push"
+        exit 1
+    fi
 
     twine upload dist/*
 
@@ -31,4 +71,8 @@ function push {
     git push --tags origin master
 }
 
-$1
+while [[ $# -ne 0 ]];
+do
+    $1
+    shift
+done
