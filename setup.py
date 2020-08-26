bb11cffe61809909b7d61d3dc01d3a0a2ef85c2b
Filippo Carra <fcarra@libertyglobal.com>
2020-08-26 22:24:56 +0200

    blacked setup.py

--- setup.py
+++ setup.py
@@ -14,35 +14,35 @@ sys.path.insert(0, here)
 
 import cdrouter
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='cdrouter',
+    name="cdrouter",
     version=cdrouter.__version__,
-    description='Python client for the CDRouter Web API',
+    description="Python client for the CDRouter Web API",
     long_description=long_description,
-    url='https://github.com/qacafe/cdrouter.py',
-    author='QA Cafe',
-    author_email='support@qacafe.com',
-    license='MIT',
+    url="https://github.com/qacafe/cdrouter.py",
+    author="QA Cafe",
+    author_email="support@qacafe.com",
+    license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        'License :: OSI Approved',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
+        "License :: OSI Approved",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
         "Environment :: Web Environment",
         "Topic :: Software Development",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    keywords='cdrouter json rest api client',
-    packages=['cdrouter'],
-    install_requires=['future', 'marshmallow', 'requests', 'requests-toolbelt']
+    keywords="cdrouter json rest api client",
+    packages=["cdrouter"],
+    install_requires=["future", "marshmallow", "requests", "requests-toolbelt"],
 )
