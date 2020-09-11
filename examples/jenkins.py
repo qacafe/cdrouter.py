0aed1754ef4b26945210ca1517109d9986d68f0c
Brad Ritchie <brad@qacafe.com>
2020-09-11 12:51:30 -0400

    Update jenkins.py script in examples/
    Added test result summary info.

--- examples/jenkins.py
+++ examples/jenkins.py
@@ -18,12 +18,13 @@ package_name = sys.argv[3]
 # create service
 c = CDRouter(base, token=token)
 
+
 p = c.packages.get_by_name(package_name)
 if not p:
     sys.exit("Package {0} not found.".format(package_name))
 
 
-print('Running package {0}'.format(package_name))
+print('Running package "{0}"'.format(package_name))
 print('')
 
 j = c.jobs.launch(Job(package_id=p.id))
@@ -33,20 +34,44 @@ while j.result_id is None:
     time.sleep(1)
     j = c.jobs.get(j.id)
 
-print('        Result-ID: {0}'.format(j.result_id))
+print('Test package launched. Job ID: {0}'.format(j.id))
+print('Result-ID: {0}'.format(j.result_id))
 print('')
 
-print('        Waiting for job to complete...')
+print('Waiting for job to complete...')
 u = c.results.updates(j.result_id, None)
 while u:
     r = c.results.get(j.result_id)
     if r.status in ['completed', 'stopped', 'error']:
         break
-    print('        Progress: {0}%'.format(u.progress.progress))
+    print('Progress: {0}%'.format(u.progress.progress))
     u = c.results.updates(j.result_id, u.id)
 
-print('        Job status: {0}'.format(r.status))
+result_url = base.strip('/') + '/results/' + str(r.id)
+print('Job status: {0}'.format(r.status))
 print('')
+
+if r.status != 'completed':
+    print('Error: test package "{0}" did not complete successfully ({1})'.format(package_name, r.status))
+    print('Test report: {0}'.format(result_url))
+    sys.exit(1)
+
+
+print('Test results:')
+print('')
+print('{0:>15} : {1}'.format('Summary',      r.result))
+print('{0:>15} : {1}'.format('Start',        r.created))
+print('{0:>15} : {1}'.format('Duration',     r.duration))
+print('{0:>15} : {1}'.format('Package',      r.package_name))
+print('{0:>15} : {1}'.format('Config',       r.config_name))
+print('{0:>15} : {1}'.format('Tags',         r.tags))
+print('{0:>15} : {1}'.format('Pass',         r.passed))
+print('{0:>15} : {1}'.format('Fail',         r.fail))
+print('')
+print('{0:>15} : {1}'.format('Test Report',  result_url))
+print('{0:>15} : {1}'.format('Test Summary', result_url + '/print'))
+print('')
+
 
 filename = 'results_{0}.xml'.format(j.result_id)
 print('Writing results to file "{0}" in Jenkins XML format'.format(filename))
@@ -60,9 +85,10 @@ with open(filename, 'w') as f:
             f.write('</failure>\n')
         elif t.result in ['pending', 'skipped']:
             f.write('        <skipped/>\n')
-            f.write('    </testcase>\n')
-            f.write('</testsuite>\n')
-            f.close()
+        f.write('    </testcase>\n')
+    f.write('</testsuite>\n')
+f.close()
+
 
 print('done')
 
